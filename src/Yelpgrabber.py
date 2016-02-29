c36de476ea545e51efffa0074fd64bd44b4b3dd7
msawangwan <misha.sawangwan@gmail.com>
2016-02-28 19:55:32 -0800

    modified script
    - added functionality to pull reviews by user
    - added functionality to pull ratings by user
    - added functionality to print reviews and ratings by user

--- src/Yelpgrabber.py
+++ src/Yelpgrabber.py
@@ -4,20 +4,36 @@ off of Yelp, for my mom.
 
 Created on Feb 25, 2016
 
-@author: austinmatthews
+@author: mishasawangwan and austinmatthews
 '''
 
 import urllib
 import urllib2
-from bs4 import BeautifulSou
+from bs4 import BeautifulSoup
 
 def main():
     theatre_one = 'http://www.yelp.com/biz/century-theatres-rowland-plaza-novato?osq=rowland+theatre'
     soup = BeautifulSoup(urllib2.urlopen(theatre_one), 'html.parser')
-    p_list = soup.find_all('p')
+    
+    desc_list = soup('p', {"itemprop":"description"})
+    user_name = soup('a', {"class":"user-display-name"})
+    ratings = soup('meta', {"itemprop":"ratingValue" })
+    
+    u_name_and_review_list = []
+    rating_list = []
+    
+    for user, review in zip(user_name, desc_list):
+        u_name_and_review_list.append('[' + user.text + ']' + '-- ' + review.text + '\n')
+        
+    for rating in ratings:
+        extracted_rating = rating["content"]
+        rating_list.append(extracted_rating)
+        
+    for c, d in zip(u_name_and_review_list, rating_list):
+        print 'Rating of [', d, '] by user', c + "\n"
+    
 
-    for d in p_list:
-        print d
+
     
 
 if __name__ == '__main__':
