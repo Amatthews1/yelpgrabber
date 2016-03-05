75758eadad53dfedf13142bfd6b27f7b6e0ca29f
amatthews1 <tylermirror@gmail.com>
2016-03-05 13:28:17 -0800

    modifed and updated script
    misha making edits on austins account
    - added dates to script
    - cleaned up some stuff

--- src/Yelpgrabber.py
+++ src/Yelpgrabber.py
@@ -11,35 +11,51 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
+# things to add: 1. add dates for all comments 
+# then we need to add the rest of the theaters.
+
 def main():
     theatre_one = 'http://www.yelp.com/biz/century-theatres-rowland-plaza-novato?osq=rowland+theatre' # page to scrap reviews and ratings from
     soup = BeautifulSoup(urllib2.urlopen(theatre_one), 'html.parser')                                 # parse html using beautiful soup  
     
     # using the format 'tag', {'attribute', 'attribute-name'} we can find specific html elements on the page
+    #theatre_name = soup('h1', {"itemprop":"name"})
     desc_list = soup('p', {"itemprop":"description"})     # creates a list of all the reviews on the page
     user_name = soup('a', {"class":"user-display-name"})  # creates a list of all the user names on the page
-    ratings = soup('meta', {"itemprop":"ratingValue" })   # creates a list of all the ratings on the page
+    overall_rate = soup('div', {"itemprop":"aggregateRating"})
+    user_meta = soup('div', {"itemprop":"review" })       # creates a list of all the ratings on the page
+    comment_date = soup('span', {"class":"rating-qualifier"})
     
     u_name_and_review_list = []                           # define lists - first one is for review by user name,
-    rating_list = []                                      # the second one is for extracted rating values
+    rating_and_user_list = []
+    date_list = []
+    user_rating_date_list = []
+    
+    theatre_name = soup.find('h1')
+    print 'threatre name:', theatre_name.text
+    print 'theatre overall rating', overall_rate[0].find("meta")["content"], '\n'
+    print 'Comments & Ratings:\n'
     
     # loop through username list and review list
     for user, review in zip(user_name, desc_list):                                        # matches corresponding review to user
         u_name_and_review_list.append('[' + user.text + ']' + '-- ' + review.text + '\n') # add both to a new list
+        
+    for user in user_meta:
+        usr = user.find_all("meta")[0]["content"]
+        usr_rate = user.find_all("meta")[1]["content"]
+        rating_and_user_list.append(usr + " gave rating of: " + usr_rate)
     
-    # loop through ratings list    
-    for rating in ratings:
-        extracted_rating = rating["content"]                                              # extract the 'content' value
-        rating_list.append(extracted_rating)                                              # add to new list
+    for d in comment_date:
+        if d.find("meta"):
+            date_list.append(d.find("meta")["content"])
+            
+    for a, b in zip(rating_and_user_list, date_list):
+        user_rating_date_list.append(a + " on date [" + b + "]")
     
-    # loop through all the lists so far    
-    for uname_and_review, rating in zip(u_name_and_review_list, rating_list):             # loop through both lists in parallel
-        print 'Rating of [', rating, '] by user', uname_and_review + '\n'                 # output final string for now
+    for a,b in zip(user_rating_date_list, u_name_and_review_list):
+        print a,b
     
-
-
     
-
 if __name__ == '__main__':
     main()
     
